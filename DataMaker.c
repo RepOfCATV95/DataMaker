5817e72075294a622f02b256e4f4db43355db163
Republic of CATV 95 <28668260+RepOfCATV95@users.noreply.github.com>
2023-09-02 15:04:54 +0000

    Code style update

--- DataMaker.c
+++ DataMaker.c
@@ -3,26 +3,26 @@
 
 int main()
 {
-	char TheData[MAX_TEXT_LENGTH];
-	char realFile[MAX_DOS_FILE];
-	FILE *FD;
+     char TheData[MAX_TEXT_LENGTH];
+     char realFile[MAX_DOS_FILE];
+     FILE *FD;
 	
-	printf("Enter a file you want to write onto:\n");
-	gets(realFile);
-	FD = fopen(realFile,"w");
-	if (FD==None)
-	{
-		printf("Failed to open a file for writing the data you entered.");
-		return 1;
-	}
+     printf("Enter a file you want to write onto:\n");
+     gets(realFile);
+     FD = fopen(realFile,"w");
+     if (FD==None)
+     {
+          printf("Failed to open a file for writing the data you entered.");
+          return 1;
+     }
 	
-	printf("Enter a text you want to write onto the file:\n");
-	gets(TheData);
+     printf("Enter a text you want to write onto the file:\n");
+     gets(TheData);
 	
-	fprintf(FD,"%s",TheData);
-	fclose(FD);
+     fprintf(FD,"%s",TheData);
+     fclose(FD);
 	
-	return 0;
+     return 0;
 }
 
    
